0002922b12c08c626b4d0caffa120c96b5e68289
Akil Aikman <soulchips@Soul-MacBook-Pro.local>
2019-06-30 12:10:21 -0400

    completed basic bot functions

--- main.go
+++ main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-	"strings"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
+	"github.com/Krognol/go-wolfram"
 	"github.com/christianrondeau/go-wit"
 	"github.com/nlopes/slack"
 )
 
 var (
-	slackClient = slack.New(os.Getenv("SLACK_ACCESS_KEY"))
-	witClient = wit.NewClient(os.Getenv("WIT_AI_ACCESS_KEY"))
+	slackClient   = slack.New(os.Getenv("SLACK_ACCESS_KEY"))
+	witClient     = wit.NewClient(os.Getenv("WIT_AI_ACCESS_KEY"))
+	wolframClient = &wolfram.Client{AppID: os.Getenv("WOLFRAM_APP_ID")}
 )
-
 
 func main() {
 	fmt.Println("starting connectin...")
@@ -25,70 +26,63 @@ func main() {
 	for msg := range rtm.IncomingEvents {
 		switch ev := msg.Data.(type) {
 		case *slack.MessageEvent:
-			fmt.Printf("Message: %v\n", ev)
-				info := rtm.GetInfo()
-				prefix := fmt.Sprintf("<@%s> ", info.User.ID)
+			info := rtm.GetInfo()
+			prefix := fmt.Sprintf("<@%s> ", info.User.ID)
 
-				if ev.User != info.User.ID && strings.HasPrefix(ev.Text, prefix) {
-					// rtm.SendMessage(rtm.NewOutgoingMessage("What's up buddy!?!?", ev.Channel))
-					handleMessage(ev)
-				}
+			if ev.User != info.User.ID && strings.HasPrefix(ev.Text, prefix) {
+				handleMessage(ev)
+			}
 		}
 	}
 }
 
-
 func handleMessage(ev *slack.MessageEvent) {
-	response, error := witClient.Message(ev.Msg.Text)
-	if error != nil {
-		log.Printf("Unable to connect to Wit.ai. Error: %v", error)
+	res, err:= witClient.Message(ev.Msg.Text)
+	if err != nil {
+		log.Printf("Unable to connect to Wit.ai. Error: %v", err)
 		return
 	}
 
 	var (
-		topEntity wit.MessageEntity
-		topEntityKey string
+		topEntity         wit.MessageEntity
+		topEntityKey      string
 		minimumConfidence = 0.5
 	)
 
-	for entityKey, entityList := range response.Entities {
+	for entityKey, entityList := range res.Entities {
 		for _, entity := range entityList {
 			if entity.Confidence > topEntity.Confidence && entity.Confidence > minimumConfidence {
 				topEntityKey = entityKey
 				topEntity = entity
 			}
 		}
-
-		// fmt.Printf("topEntity [0] %v \n", entityList[0])
 	}
-	// fmt.Printf("%v \n", response)
-	// fmt.Printf("topEntityKey %v \n", topEntityKey)
-	// fmt.Printf("topEntity %v \n", topEntity)
 
 	replyToUser(ev, topEntityKey, topEntity)
 }
 
 func replyToUser(ev *slack.MessageEvent, entityKey string, entity wit.MessageEntity) {
-	// rtm := slackClient.NewRTM()
-	// go rtm.ManageConnection()
 
-	// rtm.SendMessage(rtm.NewOutgoingMessage("What's up buddy!?!?", ev.Channel))
+	// Using the entity key, we determine the appropriate response
+	switch entityKey {
+	case "greetings":
+		slackClient.PostMessage(ev.Channel, slack.MsgOptionText("Hi there!", false), slack.MsgOptionUser(ev.User))
+		return
 
-	// fmt.Printf("%v \n", ev)
-	// fmt.Printf("%v \n", ev.User)
-	// fmt.Printf("%v \n", ev.Channel)
-	// fmt.Printf("%v \n", ev.Text)
+	case "wolfram_search_query":
+		res, err := wolframClient.GetSpokentAnswerQuery(entity.Value.(string), wolfram.Metric, 1000)
+		fmt.Printf("res: %v \n", res)
+		fmt.Printf("err: %v \n", err)
 
-	slackClient.PostMessage(ev.Channel, slack.MsgOptionText("Hello World!", false))
+		if err == nil {
+			slackClient.PostMessage(ev.Channel, slack.MsgOptionText(res, false), slack.MsgOptionUser(ev.User))
+			return
+		}
+		
+		log.Printf("unable to get data from wolfram: %v", err)
 
-	// switch entityKey {
-	// case "greetings": 
-	// 	slackClient.PostMessage("UL014TWUQ", slack.MsgOptionText("Hello world", false))
-	// 	// rtm.SendMessage(rtm.NewOutgoingMessage("What's up buddy!?!?", ev.Channel))
-	// 	return
-	// }
 
-	// slackClient.PostMessage("UL014TWUQ", slack.MsgOptionText("I dont understand", false), slack.MsgOptionAsUser(true), slack.MsgOptionUser(ev.User))
-
-	fmt.Println("...done")
+	default:
+		slackClient.PostMessage(ev.Channel, slack.MsgOptionText("I don't understand -\\__(0_0)__/-", false), slack.MsgOptionAsUser(true))
+	}
 }
